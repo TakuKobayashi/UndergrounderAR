6df17a6ebef45380e02da8f21ca0308f115785e7
TakuKobayshi <>
2018-02-19 15:25:19 +0900

    ARCore/ARKit両方に対応するために自作したツールを持ってきた

---
+++ Assets/Scripts/Common/ARControllerBase.cs
@@ -0,0 +1,34 @@
+namespace ARKitAndARCoreCommon
+{
+    using UnityEngine;
+
+    public abstract class ARControllerBase : MonoBehaviour
+    {
+        [SerializeField] protected GameObject ARMainCameraPrefab;
+        [SerializeField] protected GameObject TrackedPlanePrefab;
+        [SerializeField] private GameObject PointCloudPrefab;
+
+        protected GameObject pointCloudObj;
+        protected Camera mainCamera;
+
+        protected virtual void Awake()
+        {
+            Camera defaultCamera = Camera.main;
+            if (defaultCamera != null)
+            {
+                defaultCamera.enabled = false;
+                defaultCamera.gameObject.SetActive(false);
+            }
+            GameObject mainCameraManager = Util.InstantiateTo(this.gameObject, ARMainCameraPrefab);
+            mainCamera = Util.FindCompomentInChildren<Camera>(mainCameraManager.transform);
+            if (mainCamera == null)
+            {
+                mainCamera = mainCameraManager.GetComponent<Camera>();
+            }
+        }
+
+        protected virtual void Update()
+        {
+        }
+    }
+}
